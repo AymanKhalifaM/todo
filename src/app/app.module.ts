b9a712037f1db48be359e6e1af4c17ccf51b1f91
AymanKhalifaM <ayman.elsawy.kh94@gmail.com>
2020-11-09 14:03:37 +0200

    final todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './todo/todo.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+   MatButtonModule,
+   MatFormFieldModule,
+   FormsModule,
+   MatIconModule,
+   MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
